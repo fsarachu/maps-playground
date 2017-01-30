4f3f861b7c31de096bbc10ea813d5dbddefc15ea
fsarachu <fsarachu@gmail.com>
2017-01-30 19:15:49 -0300

    Add marker

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 var map;
 
 function initMap() {
+    // Save location
+    var pde = {lat: -34.972754, lng: -54.952095};
+
     // Create a map object and specify the DOM element for display.
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: pde,
         scrollwheel: false,
-        zoom: 8
+        zoom: 14
+    });
+
+    // Add a marker
+    var marker = new google.maps.Marker({
+        position: pde,
+        map: map,
+        title: "Punta del Este"
     });
 }
