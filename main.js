972ef3f2cf8f2b980739a86cbf5681f2f910ea46
fsarachu <fsarachu@gmail.com>
2017-01-30 20:02:51 -0300

    Rename object property

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ function initMap() {
 
     // Define locations
     var locations = [
-        {title: "Conrad Hotel", location: {lat: -34.972754, lng: -54.952095}},
-        {title: "Los Dedos", location: {lat: -34.957709, lng: -54.937577}},
-        {title: "Playa El Emir", location: {lat: -34.962126, lng: -54.940035}},
-        {title: "Faro de Punta del Este", location: {lat: -34.968930, lng: -54.951453}},
-        {title: "Plaza de Los Artesanos", location: {lat: -34.962919, lng: -54.943766}}
+        {title: "Conrad Hotel", position: {lat: -34.972754, lng: -54.952095}},
+        {title: "Los Dedos", position: {lat: -34.957709, lng: -54.937577}},
+        {title: "Playa El Emir", position: {lat: -34.962126, lng: -54.940035}},
+        {title: "Faro de Punta del Este", position: {lat: -34.968930, lng: -54.951453}},
+        {title: "Plaza de Los Artesanos", position: {lat: -34.962919, lng: -54.943766}}
     ];
 
 }
