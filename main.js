85a94ae378921e99f3b0be9221a25b926838534b
fsarachu <fsarachu@gmail.com>
2017-01-30 19:51:01 -0300

    Define multiple locations

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 var map;
 
 function initMap() {
-    // Save location
-    var pde = {lat: -34.972754, lng: -54.952095};
-
     // Create a map object and specify the DOM element for display.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.9512849, lng: -54.9452626},
@@ -11,21 +8,13 @@ function initMap() {
         zoom: 13
     });
 
-    // Add a marker
-    var marker = new google.maps.Marker({
-        position: pde,
-        map: map,
-        title: "Punta del Este"
-    });
-
-    // Add an info window
-    var infoWindow = new google.maps.InfoWindow({
-        content: "Here is Punta del Este! The overrated city where I was born!"
-    });
-
-    // Attach infoWindow to marker
-    marker.addListener('click', function () {
-        infoWindow.open(map, marker);
-    })
+    // Define locations
+    var locations = [
+        {title: "Conrad Hotel", location: {lat: -34.972754, lng: -54.952095}},
+        {title: "Los Dedos", location: {lat: -34.957709, lng: -54.937577}},
+        {title: "Playa El Emir", location: {lat: -34.962126, lng: -54.940035}},
+        {title: "Faro de Punta del Este", location: {lat: -34.968930, lng: -54.951453}},
+        {title: "Plaza de Los Artesanos", location: {lat: -34.962919, lng: -54.943766}}
+    ];
 
 }
