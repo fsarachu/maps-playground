02d3800c4b02d553fd3bba649d0a7f41e6bf87d2
fsarachu <fsarachu@gmail.com>
2017-01-31 06:53:55 -0300

    Implement sideMenu.js, menu open-close feature now working

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -1,3 +1,13 @@
 $(function () {
+    var $sideMenu = $("#side-menu");
+    var $menuToggle = $sideMenu.find(".menu-toggle");
+    var $menuToggleIcon = $menuToggle.find(".fa");
 
+    $menuToggleIcon.removeClass("fa-chevron-left").addClass("fa-chevron-right");
+    $sideMenu.addClass("closed");
+
+    $menuToggle.on('click', function () {
+        $sideMenu.toggleClass("closed");
+        $menuToggleIcon.toggleClass("fa-chevron-right fa-chevron-left");
+    });
 });
