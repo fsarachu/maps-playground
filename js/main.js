65671d8dc5b02fa3e6daf04a0146d5d0f1890426
fsarachu <fsarachu@gmail.com>
2017-02-02 20:28:24 -0300

    Fix typo

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(function () {
-    var $toggleMarkersButon = $("#toggle-markers");
+    var $toggleMarkersButton = $("#toggle-markers");
 
-    $toggleMarkersButon.on("click", function () {
+    $toggleMarkersButton.on("click", function () {
         var visibleMarkers = markers.filter(function (marker) {
             return marker.map;
         });
 
         if (visibleMarkers.length > 0) {
             hideMarkers(visibleMarkers);
-            $toggleMarkersButon.text("Show Markers");
+            $toggleMarkersButton.text("Show Markers");
         } else {
             showMarkers();
-            $toggleMarkersButon.text("Hide Markers");
+            $toggleMarkersButton.text("Hide Markers");
         }
     });
 });
