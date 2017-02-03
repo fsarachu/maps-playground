524ace835670d3d8f250308be023aed50006ddea
fsarachu <fsarachu@gmail.com>
2017-02-03 20:23:52 -0300

    Implement #toggle-drawing button click handler

--- js/main.js
+++ js/main.js
@@ -18,6 +18,12 @@ $(function () {
     var $toggleDrawingButton = $("#toggle-drawing");
 
     $toggleDrawingButton.on("click", function () {
-
+        if (drawingManager.map) {
+            drawingManager.setMap(null);
+            $toggleDrawingButton.text("Start Drawing");
+        } else {
+            drawingManager.setMap(map);
+            $toggleDrawingButton.text("Stop Drawing");
+        }
     });
 });
